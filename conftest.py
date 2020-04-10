5b859954a95da474146ba7c9e657165c4e469958
dilhoo <dilholol2@gmail.com>
2020-04-10 23:32:34 +0300

    renamed confest to conftest

---
+++ conftest.py
@@ -0,0 +1,22 @@
+import pytest
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default="chrome",
+                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default="en",
+                     help="Choose language: en,ru,es,fr, ...")
+
+                     
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    user_language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
